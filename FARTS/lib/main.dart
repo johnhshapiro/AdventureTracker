09edf76d118e5181aa17051743d4bc554fbeba56
Jake Schnorr <jschnorr@msudenver.edu>
2020-01-31 00:10:40 -0700

    buld Widget implemented, now for the mode selection buttons.

--- FARTS/lib/main.dart
+++ FARTS/lib/main.dart
@@ -4,11 +4,11 @@ void main() => runApp(BoardsEdge()); /*Loads 'main' as a Widget, even the app it
 is the single line lambda function syntax for the language. Pretty cool.  */
 
 class BoardsEdge extends StatelessWidget {
-  // This is the root Widget.
+  // This is the root Widget (Class).
   // Global variable declaration goes here I think.
 
   @override
-  Widget build(BuildContext context) { // 'build' is the equivalent of a constructor for the class (note its also a widget like everything)
+  Widget build(BuildContext context) { // 'build' is the equivalent of a constructor for a class (note its also a widget like everything)
     return MaterialApp(
       title: 'Boards Edge',
       /*theme: ThemeData(
@@ -31,6 +31,20 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    // TODO build the layout here
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'test output',
+            ),
+          ],
+        )
+        ,),
+    );
   }
 }
