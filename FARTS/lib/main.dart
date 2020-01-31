ee806f6076fb9fab69bc5c32e948b9a043f2697a
Jake Schnorr <jschnorr@msudenver.edu>
2020-01-31 00:02:35 -0700

    Root and Homepage Widgets created, need to finish homepage build widget.

--- FARTS/lib/main.dart
+++ FARTS/lib/main.dart
@@ -1,2 +1,36 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(BoardsEdge()); /*Loads 'main' as a Widget, even the app itself is a Widget. '=>' AKA 'fat arrow' in Dart 
+is the single line lambda function syntax for the language. Pretty cool.  */
+
+class BoardsEdge extends StatelessWidget {
+  // This is the root Widget.
+  // Global variable declaration goes here I think.
+
+  @override
+  Widget build(BuildContext context) { // 'build' is the equivalent of a constructor for the class (note its also a widget like everything)
+    return MaterialApp(
+      title: 'Boards Edge',
+      /*theme: ThemeData(
+        primarySwatch: Colors.black,      <--  We will likely add theme data here later. 
+      ),*/
+      home: HomePage(title: 'HomePage'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO build the layout here
+  }
+}
