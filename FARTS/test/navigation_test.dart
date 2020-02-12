91f7acbcb201072ef113bcc3a0a3c5edfd5d7b4b
Jake Schnorr <jschnorr@msudenver.edu>
2020-02-11 22:32:58 -0700

    Add test for NavBar sheets widget.

--- FARTS/test/navigation_test.dart
+++ FARTS/test/navigation_test.dart
@@ -6,9 +6,9 @@ import 'package:flutter/material.dart';
 class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
 void main() {
-  /// Test adapted from Iiro Krankka from page
-  /// https://stackoverflow.com/questions/50704647/how-to-test-navigation-via-navigator-in-flutter
-  
+  // Test adapted from Iiro Krankka from page
+  // https://stackoverflow.com/questions/50704647/how-to-test-navigation-via-navigator-in-flutter
+  // From the flutter.dev docs: https://flutter.dev/docs/cookbook/testing/widget/introduction
   group('LOGIN PAGE widget tests', () {
     testWidgets('Login button is present and triggers navigation after tapped',
     (WidgetTester tester) async {
@@ -45,11 +45,7 @@ void main() {
       await tester.tap(find.text('New User'));
       await tester.pumpAndSettle();
 
-      /// Verify that a push event happened
       verify(mockObserver.didPush(any, any));
-
-      /// You'd also want to be sure that your page is now
-      /// present in the screen.
       expect(find.byType(CreateNewUser), findsOneWidget); 
     });
   });
@@ -61,8 +57,8 @@ void main() {
         final mockObserver = MockNavigatorObserver();
         await tester.pumpWidget(
           MaterialApp(
-          home: CreateNewUser(),
-          navigatorObservers: [mockObserver],
+            home: CreateNewUser(),
+            navigatorObservers: [mockObserver],
           ),
         );
 
@@ -70,16 +66,45 @@ void main() {
         await tester.tap(find.text('Create'));
         await tester.pumpAndSettle();
 
-        /// Verify that a push event happened
         verify(mockObserver.didPush(any, any));
-
-        /// You'd also want to be sure that your page is now
-        /// present in the screen.
         expect(find.byType(CreateNewUser), findsOneWidget); 
     });
   });
 
-  group('SELECT MODE PAGE widget tests', () {
-    
+  group('SELECT MODE PAGE NAVBAR widget tests', () {
+    testWidgets('NavBar Home Button test',
+      (WidgetTester tester) async {
+        final mockObserver = MockNavigatorObserver();
+        await tester.pumpWidget(
+          MaterialApp(
+            home: MainMenuPage(),
+            navigatorObservers: [mockObserver],
+          ),
+        );
+
+        expect(find.text('Home'), findsOneWidget);
+        await tester.tap(find.text('Home'));
+        await tester.pumpAndSettle();
+
+        verify(mockObserver.didPush(any, any));
+        expect(find.byType(MainMenuPage), findsOneWidget);
+    });
+    testWidgets('NavBar Sheets Button test',
+      (WidgetTester tester) async {
+        final mockObserver = MockNavigatorObserver();
+        await tester.pumpWidget(
+          MaterialApp(
+            home: MainMenuPage(),
+            navigatorObservers: [mockObserver],
+          ),
+        );
+
+        expect(find.text('Sheets'), findsOneWidget);
+        await tester.tap(find.text('Sheets'));
+        await tester.pumpAndSettle();
+
+        verify(mockObserver.didPush(any, any));
+        expect(find.byType(MainMenuPage), findsOneWidget);
+    });
   });
 }
