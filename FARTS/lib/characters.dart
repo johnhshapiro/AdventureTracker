1bd1e1a457c0dbad3f8ac8197fa6dd8f3a8dad51
G-Leib <gleibovi@msudenver.edu>
2020-02-16 18:16:47 -0700

    just getting started on the character view

---
+++ FARTS/lib/characters.dart
@@ -0,0 +1,11 @@
+class CharacterSelect extends StatefulWidget {
+  CharacterSelect({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _CharacterSelectState createState() => _CharacterSelectState();
+}
+
+class _CharacterSelectState extends State<CharacterSelect> {
+  
+}
