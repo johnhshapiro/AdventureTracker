9f752abe9c3b96f21acb225262ee83df38f74575
Jake Schnorr <jschnorr@msudenver.edu>
2020-02-18 23:43:46 -0700

    oops forgot to point app back at login page on startup.

--- FARTS/lib/main.dart
+++ FARTS/lib/main.dart
@@ -20,7 +20,7 @@ class BoardsEdge extends StatelessWidget {
   Widget build(BuildContext context) {
     // 'build' is the equivalent of a constructor for a class (note its also a widget like everything)
     return MaterialApp(
-        home: HomePage(), // Sets the login page as the home page.
+        home: LoginPage(), // Sets the login page as the home page.
         theme: ThemeData(
           // Define the default brightness and colors.
           brightness: Brightness.dark,
