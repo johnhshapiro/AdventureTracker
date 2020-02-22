bb31786fedce33b3193a29f0ebf015350625e561
G-Leib <gleibovi@msudenver.edu>
2020-02-22 14:47:08 -0700

    pulling characters from db

--- FARTS/lib/characters.dart
+++ FARTS/lib/characters.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future<List<String>> _getChars() async {
+  final QuerySnapshot characterDocs = await Firestore.instance.collection('users').getDocuments();
+  final List<DocumentSnapshot> characterDocList = characterDocs.documents;
+
+  return characterDocList.map((doc) => doc['name']);
+}
+
 
 class CharacterSelect extends StatefulWidget {
   CharacterSelect({Key key, this.title}) : super(key: key);
@@ -11,7 +20,9 @@ class CharacterSelect extends StatefulWidget {
 
 class _CharacterSelectState extends State<CharacterSelect> {
   // TODO: Replace example characters with saved characters
-  final List<String> chars = <String>['Gnome Fighter', 'Half-Orc Wizard', 'Goblin Cleric'];
+  
+  final Future<List<String>> charsFuture = _getChars();
+  
 
   @override
   Widget build(BuildContext context) {
@@ -22,42 +33,73 @@ class _CharacterSelectState extends State<CharacterSelect> {
           Image(
             image: AssetImage("assets/knight.jpg"),
             fit: BoxFit.cover,
-            color: Colors.black87,
+            color: Colors.black38,
             colorBlendMode: BlendMode.luminosity
           ),
-          ListView.separated(
-            padding: const EdgeInsets.all(50),
-            itemCount: chars.length + 1,
-            itemBuilder: (BuildContext contex, int index) {
-              if (index == chars.length)
-                return Container(
-                  height: 50,
-                  color: Colors.grey,
-                  child: Center(child: Text('Create New'))
-                );
-              else
-                return Container(
-                  height: 50,
-                  color: Colors.grey,
-                  child: Center(child: Text('${chars[index]}'))
-                );
-            },
-            separatorBuilder: (BuildContext context, int index) => const Divider()
-          )
+          FutureBuilder<List<String>>(
+            future: charsFuture,
+            builder: (context, snapshot) {
+              if(snapshot.connectionState != ConnectionState.done) {
+                return Text("Loading");
+              }
+              if(snapshot.hasError) {
+                return Text("Error");
+              }
+              List<String> chars = snapshot.data ?? [];
+              return ListView.separated(
+                padding: const EdgeInsets.all(50),
+                itemCount: chars.length,
+                itemBuilder: (BuildContext contex, int index) {
+                  String name = chars[index];
+                  if (index == chars.length)
+                    return Container(
+                      height: 50,
+                      color: Colors.grey,
+                      child: Center(child: Text('Create New'))
+                    );
+                  else
+                    return Container(
+                      height: 50,
+                      color: Colors.grey,
+                      child: Center(child: Text(name))
+                    );
+                },
+                separatorBuilder: (BuildContext context, int index) => const Divider()
+              );
+          })
+          // ListView.separated(
+          //   padding: const EdgeInsets.all(50),
+          //   itemCount: chars.length + 1,
+          //   itemBuilder: (BuildContext contex, int index) {
+          //     if (index == chars.length)
+          //       return Container(
+          //         height: 50,
+          //         color: Colors.grey,
+          //         child: Center(child: Text('Create New'))
+          //       );
+          //     else
+          //       return Container(
+          //         height: 50,
+          //         color: Colors.grey,
+          //         child: Center(child: Text('${chars[index]}'))
+          //       );
+          //   },
+          //   separatorBuilder: (BuildContext context, int index) => const Divider()
+          // )
         ]
       )
     );
   }
 }
 
-class CharacterSheetPage extends StatefulWidget {
-  CharacterSheetPage({Key key, this.title}) : super(key: key);
-  final String title;
+// class CharacterSheetPage extends StatefulWidget {
+//   CharacterSheetPage({Key key, this.title}) : super(key: key);
+//   final String title;
 
-  @override
-  _CharacterSheetPageState createState() => _CharacterSheetPageState();
-}
+//   @override
+//   _CharacterSheetPageState createState() => _CharacterSheetPageState();
+// }
 
-class _CharacterSheetPageState extends State<CharacterSheetPage> {
-  @override
-}
+// class _CharacterSheetPageState extends State<CharacterSheetPage> {
+//   // @override
+// }
