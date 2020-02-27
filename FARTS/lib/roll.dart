ee31477ee99f27cca92f2d18d3d0d4eaf6946b40
Jake Schnorr <jschnorr@msudenver.edu>
2020-02-26 20:46:43 -0700

    Fixed RollPage() bug. First version of page done.

--- FARTS/lib/roll.dart
+++ FARTS/lib/roll.dart
@@ -4,22 +4,19 @@ import 'package:FARTS/vibrate.dart';
 
 class Roll {
   final _random = Random();
+  // TODO TRY USING ASSERTIONS to check parameters and values
 
-  // TODO check for bad parameter passing, throw meaningful errors. TRY USING ASSERTIONS
-
-  // TODO take more paramters for number of dice (put shorthand function in a loop to achieve this)
 
   // You can call this Shorthand function by calling Roll().rollInRange(int minRange, int maxRange)
   // Returns a positive random integer uniformly distributed on the range from min inclusive to max exclusive.
   // *** NOTE *** if u want to roll a d6 you would pass 1 and 7 as the values NOT 1 and 6.
   int rollInRange(int minInclusive, int maxExclusive) => minInclusive + _random.nextInt(maxExclusive - minInclusive);
 
-
   int rollMultipleInRange(int minInclusive, int maxExclusive, int numDice, int addToTotal) {
       int _rollTotal = 0;
 
       if (numDice < 0 || maxExclusive < minInclusive || addToTotal == null) {
-        throw Exception('Error: called rollMultipleInRange with incorrect paramters');
+        throw Exception('Error: called rollMultipleInRange with incorrect parameters');
       }
 
       for (int i = 0; i < numDice; i ++) {
--- FARTS/lib/rollpage.dart
+++ FARTS/lib/rollpage.dart
@@ -11,26 +11,12 @@ class RollPage extends StatefulWidget {
 }
 
 class _RollPageState extends State<RollPage> {
-  int _totalNumDice = 0;
   int _totalRollValue = 0;
   int _listItemCount = 1;
-  int _d4Count = 0;
-  int _d6Count = 0;
-  int _d10Count = 0;
-  int _d12Count = 0;
-  int _d20Count = 0;
-  int _d100Count = 0;
-  int _dnCount = 0;
-  int _d2Count = 0;
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        centerTitle: true,
-        title: Text(_totalRollValue.toString(), style: TextStyle(fontSize: 50),),
-      ),
       floatingActionButton: GestureDetector(
         onLongPress: () {
           setState(() {
@@ -42,19 +28,29 @@ class _RollPageState extends State<RollPage> {
         },
         child: FloatingActionButton(
           onPressed: (){
-              _addListItem();
+              setState(() {
+                _listItemCount ++;  
+              });
           },
           child: Icon(Icons.add),
         ),
       ),
       body: Column(
         children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 45),
+            height: 90,
+            child: Text(_totalRollValue.toString(), style: TextStyle(fontSize: 75, color: Colors.red),),
+      ),
           Expanded(
             child: ListView.builder(
               itemCount: _listItemCount,
               // Adds more list items dynamically by calling the build function.
               itemBuilder: (BuildContext context, int index) {
-                return _buildDiceBody(index);
+                // Creates a new instance of the DiceBag() class every time _listItemCount is incremented.
+                // Provides _updateRollTotal as a callback function so the DiceBag() child can update the
+                // the total roll displayed in the parent. 
+                return new DiceBag(parentAction: _updateRollTotal);
               }
             ),
           ),
@@ -63,7 +59,40 @@ class _RollPageState extends State<RollPage> {
     );
   }
 
-  _buildDiceBody(int index) {
+  // This function is passed as a callback to the DiceBag() child.
+  _updateRollTotal(int totalFromChild) { 
+    setState(() {
+      _totalRollValue = totalFromChild;
+    });
+  }
+
+}
+
+
+
+// Companion Class enables each row of dice widgets to be instantiated on demand and keep track of their own dice values.
+class DiceBag extends StatefulWidget{
+  final ValueChanged<int> parentAction;
+  final String title;
+  const DiceBag({Key key, this.title, this.parentAction}) : super(key: key);
+
+  @override
+  _DiceBagState createState() => _DiceBagState();
+}
+
+class _DiceBagState extends State<DiceBag> {
+  int _totalNumDice = 0;
+  int _totalRollValue = 0;
+  int _d4Count = 0;
+  int _d6Count = 0;
+  int _d10Count = 0;
+  int _d12Count = 0;
+  int _d20Count = 0;
+  int _d100Count = 0;
+  int _dnCount = 0;
+  int _d2Count = 0;
+
+  Widget build(BuildContext context) {
     return new Container(
       height: 80,
       padding: EdgeInsets.all(5),       
@@ -97,12 +126,14 @@ class _RollPageState extends State<RollPage> {
                   onPressed: () {
                     setState(() {
                       _d4Count ++;
+                      _totalNumDice ++;
                     });
                   },
                   onLongPress: () {
                     setState(() {
                       if (_d4Count > 0) {
                         _d4Count --;
+                        _totalNumDice --;
                       }
                     });
                   },
@@ -140,12 +171,14 @@ class _RollPageState extends State<RollPage> {
                   onPressed: () {
                     setState(() {
                       _d6Count ++;
+                      _totalNumDice ++;
                     });
                   },
                   onLongPress: () {
                     setState(() {
                       if (_d6Count > 0) {
                         _d6Count --;
+                        _totalNumDice --;
                       }
                     });
                   },
@@ -183,12 +216,14 @@ class _RollPageState extends State<RollPage> {
                   onPressed: () {
                     setState(() {
                       _d10Count ++;
+                      _totalNumDice ++;
                     });
                   },
                   onLongPress: () {
                     setState(() {
                       if (_d10Count > 0) {
                         _d10Count --;
+                        _totalNumDice --;
                       }
                     });
                   },
@@ -230,12 +265,14 @@ class _RollPageState extends State<RollPage> {
                   onPressed: () {
                     setState(() {
                       _d12Count ++;
+                      _totalNumDice ++;
                     });
                   },
                   onLongPress: () {
                     setState(() {
                       if (_d12Count > 0) {
                         _d12Count --;
+                        _totalNumDice --;
                       }
                     });
                   },
@@ -273,12 +310,14 @@ class _RollPageState extends State<RollPage> {
                   onPressed: () {
                     setState(() {
                       _d20Count ++;
+                      _totalNumDice ++;
                     });
                   },
                   onLongPress: () {
                     setState(() {
                       if (_d20Count > 0) {
                         _d20Count --;
+                        _totalNumDice --;
                       }
                     });
                   },
@@ -312,22 +351,12 @@ class _RollPageState extends State<RollPage> {
           ),
         ],
       ),
-    );
+    ); 
   }
-
-  _addListItem() {
-    setState(() {
-      _listItemCount ++;
-    });
-  }
-
   // This is called when u press the golden die on the left of each dice row, it returns the int total pip value.
   _rollDice() {
     setState(() {
     _totalRollValue = 0;
-    _totalNumDice = _d2Count + _d4Count + _d6Count + _d10Count + _d12Count + _d20Count + _d100Count + _dnCount;
-    // TODO try keeping track of _dNUMBER count variables here to fix bug (renitizalize when function is called).
-
     // Capture d2 total
     _totalRollValue += Roll().rollMultipleInRange(1, 3, _d2Count, 0);
     // Capture d4 total
@@ -343,18 +372,15 @@ class _RollPageState extends State<RollPage> {
     // Capture d100 total
     _totalRollValue += Roll().rollMultipleInRange(1, 101, _d100Count, 0);
 
-    // TODO create a local variable to hold the dn face value and then pass this to the Roll class as a param.
-    // Return dn total
-    // for (int i = 0; i < _dnCount; i ++) {
-    //   _totalRollValue += Roll().rollInRange(1, 5);
-    // }
+    // TODO create a local variable to hold the dn face value and then pass this to the rollMultipeInRange() as a param.
+
 
       // Trigger vibration 'dice roll' feedback based on how many dice are being rolled.
       if(_totalNumDice > 0 && _totalNumDice < 5) { Vibrate().smallRoll();}
       if(_totalNumDice >= 5 && _totalNumDice < 10) { Vibrate().bigRoll();}
       if(_totalNumDice >= 10) { Vibrate().epicRoll();}
-  
-    return _totalRollValue;
+
+    widget.parentAction(_totalRollValue);
     });
   }
 
