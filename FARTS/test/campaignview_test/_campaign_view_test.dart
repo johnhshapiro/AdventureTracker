5975ba2b54f627b771dd35b9035746611fcbf138
fernando <fernandobabonoyaba@fernandos-MacBook-Pro.local>
2020-04-13 17:33:17 -0600

    fix campaign view test error

--- FARTS/test/campaignview_test/_campaign_view_test.dart
+++ FARTS/test/campaignview_test/_campaign_view_test.dart
@@ -2,28 +2,30 @@ import 'package:flutter/material.dart';
 import "package:flutter_test/flutter_test.dart";
 
 // Relevant pages
-import 'package:FARTS/campaignview/viewcampaignpage.dart';
+import 'package:FARTS/campaignview/campaign_view.dart';
 
 void main() {
   group('View Campaign page widget tests', () {
-
-    testWidgets('View campaign page widgets are present', (WidgetTester tester) async {
-      MaterialApp app = MaterialApp(home: Campaign());
+    testWidgets('View campaign page widgets are present',
+        (WidgetTester tester) async {
+      MaterialApp app = MaterialApp(home: CampaignView());
       await tester.pumpWidget(app);
 
       // Make sure these widgets are on the view campaign page.
-      expect(find.byType(Campaign), findsOneWidget);
+      expect(find.byType(CampaignView), findsOneWidget);
       expect(find.byType(Container), findsWidgets);
       expect(find.byType(Scaffold), findsWidgets);
     });
 
     testWidgets('View Campaign Text is present', (WidgetTester tester) async {
-      MaterialApp app = MaterialApp(home: Scaffold(body: Campaign(),),);
+      MaterialApp app = MaterialApp(
+        home: Scaffold(
+          body: CampaignView(),
+        ),
+      );
       await tester.pumpWidget(app);
 
       // Make sure all the text is showing correctly.
     });
-
   });
-
 }
