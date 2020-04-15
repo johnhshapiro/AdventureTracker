876fffcdc11d7b86b53d5f914aca867dccc61e56
johnhshapiro <jshapir4@msudenver.edu>
2020-04-15 15:37:48 -0600

    removed unused imports

--- FARTS/lib/main.dart
+++ FARTS/lib/main.dart
@@ -6,10 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-
-import './loginpage.dart';
-// import './homepage.dart';
 
 void main() {
   runApp(BoardsEdge());
