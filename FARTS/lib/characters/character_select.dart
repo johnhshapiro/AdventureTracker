27f147a1909254211b9c6f99b1e6cc9783b597d3
G-Leib <gleibovi@msudenver.edu>
2020-04-20 16:17:37 -0600

    character select page now matches campaign select page

--- FARTS/lib/characters/character_select.dart
+++ FARTS/lib/characters/character_select.dart
@@ -6,6 +6,7 @@ import 'package:FARTS/characters/new_character.dart';
 import 'package:FARTS/characters/character_service.dart';
 import 'package:FARTS/characters/character_model.dart';
 import 'package:FARTS/models/user_model.dart';
+import 'package:FARTS/custom_scaffold.dart';
 
 class CharacterList extends StatefulWidget {
   @override
@@ -17,66 +18,70 @@ class _CharacterListState extends State<CharacterList> {
   Widget build(BuildContext context) {
     final chars = Provider.of<List<Character>>(context) ?? [];
 
-    return Container(
-        child: Stack(fit: StackFit.expand, children: <Widget>[
-      Image(
-          image: AssetImage("assets/knight.jpg"),
-          fit: BoxFit.cover,
-          color: Colors.black38,
-          colorBlendMode: BlendMode.luminosity),
-      ListView.separated(
-          padding: const EdgeInsets.all(50),
-          itemCount: chars.length + 1,
-          itemBuilder: (BuildContext contex, int index) {
-            if (index == chars.length)
-              return MaterialButton(
-                  child: Text('Create New'),
-                  color: Colors.grey[800],
-                  splashColor: Colors.amber,
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => CreateNewCharacter()),
-                    );
-                  });
-            else
-              return MaterialButton(
-                  child: Text('${chars[index].name}'),
-                  color: Colors.grey[800],
-                  splashColor: Colors.amber,
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              CharacterSheetPage(character: chars[index])),
-                    );
-                  });
-          },
-          separatorBuilder: (BuildContext context, int index) =>
-              const Divider())
-    ]));
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          fit: StackFit.expand, 
+          children: <Widget>[
+            Image(
+                  image: AssetImage('assets/realoldpaper.jpg'),
+                  fit: BoxFit.cover,
+            ),
+            ListView.separated(
+              padding: const EdgeInsets.all(50),
+              itemCount: chars.length,
+              itemBuilder: (BuildContext contex, int index) {
+                return Card(
+                  color: Colors.transparent,
+                  child: ListTile(
+                    title: Text('${chars[index].name}',
+                      style: TextStyle(
+                      fontSize: 16.0, color: Colors.black)),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                CharacterSheetPage(character: chars[index])),
+                      );
+                    }
+                  ),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider()
+            )
+          ]
+        )
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: Text('Create New'),
+        backgroundColor: Colors.grey[500],
+      ),
+    );
   }
 }
 
 class CharacterSelect extends StatefulWidget {
-  CharacterSelect({Key key, this.title, @required this.userData})
-      : super(key: key);
-  final String title;
-  final UserData userData;
+    CharacterSelect({Key key, this.title, @required this.userData})
+        : super(key: key);
+    final String title;
+    final UserData userData;
 
-  @override
-  _CharacterSelectState createState() => _CharacterSelectState();
+    @override
+    _CharacterSelectState createState() => _CharacterSelectState();
 }
 
 class _CharacterSelectState extends State<CharacterSelect> {
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<List<Character>>.value(
-      value: CharacterService(userId: widget.userData.uid).chars,
-      child: Scaffold(
-        body: CharacterList() ?? [],
+    return CustomScaffold(
+      body: StreamProvider<List<Character>>.value(
+        value: CharacterService(userId: widget.userData.uid).chars,
+        child: Scaffold(
+          body: CharacterList() ?? [],
+        ),
       ),
     );
   }
