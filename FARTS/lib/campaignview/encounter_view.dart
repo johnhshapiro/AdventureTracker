cd51321494540cf919edffe0515ea4297a5815f0
johnhshapiro <jshapir4@msudenver.edu>
2020-04-29 14:25:48 -0600

    encounterViewWidget is not called with showCorrectWidget()

--- FARTS/lib/campaignview/encounter_view.dart
+++ FARTS/lib/campaignview/encounter_view.dart
@@ -1,4 +1,5 @@
 import 'package:FARTS/models/campaign_model.dart';
+import 'package:FARTS/services/stream.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -13,64 +14,37 @@ class _EncounterViewState extends State<EncounterView> {
   @override
   Widget build(BuildContext context) {
     _campaignModelStream = Provider.of<CampaignModel>(context);
-    if (_campaignModelStream == null){
-      print("WAITING FOR DATA TO LOAD");
-      return Center(child: CircularProgressIndicator(),);
-    } else {
-      print("DATA IS LOADED!");
-          return Scaffold(
-        body: Stack(
-      fit: StackFit.expand,
-      children: <Widget>[
-        Image(
-          image: AssetImage('assets/realoldpaper.jpg'),
-          fit: BoxFit.fill,
-        ),
-        ListView.builder(
-            itemCount: _campaignModelStream.encounters.length,
-            itemBuilder: (context, index) {
-              return Card(
-                color: Colors.transparent,
-                child: ListTile(
-                  trailing: Text("example XP",
-                      style: TextStyle(fontSize: 12.0, color: Colors.black)),
-                  title: Text(_campaignModelStream.encounters[index],
-                      style: TextStyle(color: Colors.black)),
-                  onTap:
-                      () {}, // TODO inflate or navigate to the corresponding encounter data.
-                ),
-              );
-            }),
-      ],
-    ));
-    }
+    return CampaignModelStream().showCorrectWidget(
+      _campaignModelStream,
+      encounterViewWidget(_campaignModelStream),
+    );
   }
 }
 
 Widget encounterViewWidget(CampaignModel campaign) {
   return Scaffold(
-        body: Stack(
-      fit: StackFit.expand,
-      children: <Widget>[
-        Image(
-          image: AssetImage('assets/realoldpaper.jpg'),
-          fit: BoxFit.fill,
-        ),
-        ListView.builder(
-            itemCount: campaign.encounters.length,
-            itemBuilder: (context, index) {
-              return Card(
-                color: Colors.transparent,
-                child: ListTile(
-                  trailing: Text("example XP",
-                      style: TextStyle(fontSize: 12.0, color: Colors.black)),
-                  title: Text(campaign.encounters[index],
-                      style: TextStyle(color: Colors.black)),
-                  onTap:
-                      () {}, // TODO inflate or navigate to the corresponding encounter data.
-                ),
-              );
-            }),
-      ],
-    ));
+      body: Stack(
+    fit: StackFit.expand,
+    children: <Widget>[
+      Image(
+        image: AssetImage('assets/realoldpaper.jpg'),
+        fit: BoxFit.fill,
+      ),
+      ListView.builder(
+          itemCount: campaign.encounters.length,
+          itemBuilder: (context, index) {
+            return Card(
+              color: Colors.transparent,
+              child: ListTile(
+                trailing: Text("example XP",
+                    style: TextStyle(fontSize: 12.0, color: Colors.black)),
+                title: Text(campaign.encounters[index],
+                    style: TextStyle(color: Colors.black)),
+                onTap:
+                    () {}, // TODO inflate or navigate to the corresponding encounter data.
+              ),
+            );
+          }),
+    ],
+  ));
 }
