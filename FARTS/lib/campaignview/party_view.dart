694d31518b2a0ed4d51f280eab7f4c119dad5d15
johnhshapiro <jshapir4@msudenver.edu>
2020-04-29 13:31:39 -0600

    extracted party view widget

--- FARTS/lib/campaignview/party_view.dart
+++ FARTS/lib/campaignview/party_view.dart
@@ -15,11 +15,14 @@ class _PartyViewState extends State<PartyView> {
     // Initializes the stream of data for this specifici campaign, mapped from firestore to the local CampaignModel
     _campaignModelStream = Provider.of<CampaignModel>(context);
     if (_campaignModelStream == null){
-      print("WAITING FOR DATA TO LOAD");
       return Center(child: CircularProgressIndicator(),);
     } else {
-      print("DATA IS LOADED!");
-      return Scaffold(
+    }
+  }
+}
+
+Widget partyViewWidget(CampaignModel campaign) {
+  return Scaffold(
           body: Stack(
         fit: StackFit.expand,
         children: <Widget>[
@@ -28,7 +31,7 @@ class _PartyViewState extends State<PartyView> {
             fit: BoxFit.fill,
           ),
           ListView.builder(
-              itemCount: _campaignModelStream.party.length,
+              itemCount: campaign.party.length,
               itemBuilder: (context, index) {
                 return Card(
                   color: Colors.transparent,
@@ -36,7 +39,7 @@ class _PartyViewState extends State<PartyView> {
                     leading: Icon(Icons.person),
                     trailing: Text("Lvl Class Name",
                         style: TextStyle(fontSize: 12.0, color: Colors.black)),
-                    title: Text(_campaignModelStream.party[index],
+                    title: Text(campaign.party[index],
                         style: TextStyle(color: Colors.black)),
                     onTap:
                         () {}, // TODO navigate to the corresponding characterpage (index can be used to retrieve the chars like 'where(DBreference = index) ....showcharacter...)
@@ -45,6 +48,4 @@ class _PartyViewState extends State<PartyView> {
               }),
         ],
       ));
-    }
-  }
 }
