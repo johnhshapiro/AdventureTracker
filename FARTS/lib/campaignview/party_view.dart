41179985b5da49f5451c0b6afe3fb15d881e8d87
johnhshapiro <jshapir4@msudenver.edu>
2020-04-29 13:36:55 -0600

    page now displays using showCorrectWidget()

--- FARTS/lib/campaignview/party_view.dart
+++ FARTS/lib/campaignview/party_view.dart
@@ -1,4 +1,5 @@
 import 'package:FARTS/models/campaign_model.dart';
+import 'package:FARTS/services/stream.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,38 +15,38 @@ class _PartyViewState extends State<PartyView> {
   Widget build(BuildContext context) {
     // Initializes the stream of data for this specifici campaign, mapped from firestore to the local CampaignModel
     _campaignModelStream = Provider.of<CampaignModel>(context);
-    if (_campaignModelStream == null){
-      return Center(child: CircularProgressIndicator(),);
-    } else {
-    }
+    return CampaignModelStream().showCorrectWidget(
+      _campaignModelStream,
+      partyViewWidget(_campaignModelStream),
+    );
   }
 }
 
 Widget partyViewWidget(CampaignModel campaign) {
   return Scaffold(
-          body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Image(
-            image: AssetImage('assets/realoldpaper.jpg'),
-            fit: BoxFit.fill,
-          ),
-          ListView.builder(
-              itemCount: campaign.party.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  color: Colors.transparent,
-                  child: ListTile(
-                    leading: Icon(Icons.person),
-                    trailing: Text("Lvl Class Name",
-                        style: TextStyle(fontSize: 12.0, color: Colors.black)),
-                    title: Text(campaign.party[index],
-                        style: TextStyle(color: Colors.black)),
-                    onTap:
-                        () {}, // TODO navigate to the corresponding characterpage (index can be used to retrieve the chars like 'where(DBreference = index) ....showcharacter...)
-                  ),
-                );
-              }),
-        ],
-      ));
+      body: Stack(
+    fit: StackFit.expand,
+    children: <Widget>[
+      Image(
+        image: AssetImage('assets/realoldpaper.jpg'),
+        fit: BoxFit.fill,
+      ),
+      ListView.builder(
+          itemCount: campaign.party.length,
+          itemBuilder: (context, index) {
+            return Card(
+              color: Colors.transparent,
+              child: ListTile(
+                leading: Icon(Icons.person),
+                trailing: Text("Lvl Class Name",
+                    style: TextStyle(fontSize: 12.0, color: Colors.black)),
+                title: Text(campaign.party[index],
+                    style: TextStyle(color: Colors.black)),
+                onTap:
+                    () {}, // TODO navigate to the corresponding characterpage (index can be used to retrieve the chars like 'where(DBreference = index) ....showcharacter...)
+              ),
+            );
+          }),
+    ],
+  ));
 }
