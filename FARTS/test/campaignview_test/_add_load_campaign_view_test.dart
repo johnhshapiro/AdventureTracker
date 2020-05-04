e40e94b4e41aa34aa2c1461fcb5f610c3f24cf3f
johnhshapiro <jshapir4@msudenver.edu>
2020-05-03 18:06:37 -0600

    added navigation test for tapping a campaign

--- FARTS/test/campaignview_test/_add_load_campaign_view_test.dart
+++ FARTS/test/campaignview_test/_add_load_campaign_view_test.dart
@@ -1,11 +1,14 @@
 import 'package:FARTS/campaignview/add_load_campaign_view.dart';
+import 'package:FARTS/campaignview/campaign_view.dart';
 import 'package:FARTS/models/user_model.dart';
 import 'package:FARTS/services/stream.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore_mocks/cloud_firestore_mocks.dart';
+import 'package:mockito/mockito.dart';
+
+class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
 main() {
   group('showCorrectWidgetFunctions correctly', () {
@@ -33,9 +36,12 @@ main() {
       // Building mock data with which to test
       await instance.collection('campaigns').document('M8OTV0XlKcW6l4YhGyUCNY74Sfj1').setData({
         'name': 'Shrek it up'
+        ''
       });
+      final mockObserver = MockNavigatorObserver();
       final snapshotData = await instance.collection('campaigns').getDocuments();
       MaterialApp app = MaterialApp(
+        navigatorObservers: [mockObserver],
         home: Builder(
             builder:
                 (BuildContext context) {        
@@ -44,6 +50,11 @@ main() {
       );
       await tester.pumpWidget(app);
       await tester.tap(find.text("Shrek it up"));
+      // Two pumps because pump and settle times out...
+      await tester.pump();
+      await tester.pump();
+      verify(mockObserver.didPush(any, any));
+      expect(find.byType(CampaignView), findsOneWidget);
     });
   });
 }
