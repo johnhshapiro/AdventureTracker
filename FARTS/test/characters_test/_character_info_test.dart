c90eeb50c7b98cc51d49c1aa2ce41021b0ccdbd6
fbabonoy <fbabonoy@msudenver.edu>
2020-05-05 19:20:03 -0600

    added test for hidden dice 100

--- FARTS/test/characters_test/_character_info_test.dart
+++ FARTS/test/characters_test/_character_info_test.dart
@@ -1,11 +1,28 @@
 import 'package:FARTS/characters/character_model.dart';
+import 'package:FARTS/models/campaign_model.dart';
+import 'package:FARTS/models/user_model.dart';
+import 'package:FARTS/services/database.dart';
+import 'package:FARTS/services/stream.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import "package:flutter_test/flutter_test.dart";
 
 // Relevant pages
 import 'package:FARTS/characters/character_info.dart';
 
-void main() {
+import 'package:cloud_firestore_mocks/cloud_firestore_mocks.dart';
+import 'package:provider/provider.dart';
+
+Future<void> main() async {
+  final instance = MockFirestoreInstance();
+  await instance.collection('users').add({
+    'name': 'Bob',
+  });
+  final snapshot = await instance.collection('users').getDocuments();
+  print(snapshot.documents.length); // 1
+  print(snapshot.documents.first['username']); // 'Bob'
+  print(instance.dump());
+
   testWidgets(
     'test for drawer Mode',
     (WidgetTester tester) async {
@@ -29,7 +46,7 @@ void main() {
         experiencePoints: '12',
         inventory: '12',
         languages: 'asd',
-        name: 'name',
+        name: 'Bob',
         notes: 'asfadf',
         race: 'dsdfgs',
         skills: 'afd',
@@ -38,8 +55,15 @@ void main() {
         userId: 'user',
       );
       await tester.pumpWidget(MaterialApp(
-        home: CharacterInfoPage(
-          character: char,
+        home: StreamProvider<UserData>.value(
+          value: DatabaseService().userData,
+          child: StreamProvider<CampaignModel>.value(
+            value: CampaignModelStream()
+                .streamCampaignData(snapshot.documents.first),
+            child: CharacterInfoPage(
+              character: char,
+            ),
+          ),
         ),
       ));
       expect(find.byType(Scaffold), findsOneWidget);
